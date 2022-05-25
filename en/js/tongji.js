15f3c8a6ffb37c4ce9475fc36f01e81d2103bc31
jimmy <JimmyMall@live.com>
2022-05-25 11:13:40 +0800

    en0.4.0-2 调整统计代码 [May 25th, 2022 Wed.]

--- en/js/tongji.js
+++ en/js/tongji.js
@@ -1,8 +1,1 @@
-//百度统计
-var _hmt = _hmt || [];
-(function() {
-	var hm = document.createElement("script");
-	hm.src = "https://hm.baidu.com/hm.js?e8bfc4624d292f16f30358abd921d748";
-	var s = document.getElementsByTagName("script")[0]; 
-	s.parentNode.insertBefore(hm, s);
-})();
+console.log("Can delete now; from ielts.. /en/js/tongji.js")
---
+++ static/js/tongji.js
@@ -0,0 +1,8 @@
+//百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4910b270b579bb26c475d6927293d16f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
